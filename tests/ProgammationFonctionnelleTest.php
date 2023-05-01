9b649fbd3b343f64c2e72aa8af332f1018ee564e
Baptiste Langlade <baptouuuu@gmail.com>
2023-05-01 16:30:04 +0200

    ajout du premier exercice

--- tests/ProgammationFonctionnelleTest.php
+++ tests/ProgammationFonctionnelleTest.php
@@ -4,26 +4,54 @@ declare(strict_types = 1);
 namespace Tests\Kata;
 
 use PHPUnit\Framework\TestCase;
+use Innmind\BlackBox\{
+    PHPUnit\BlackBox,
+    Set,
+};
 
 class ProgammationFonctionnelleTest extends TestCase
 {
-    private \Generator $csv;
+    use BlackBox;
+
+    private $csv1;
+    private $csv2;
 
     public function setUp(): void
     {
-        $this->csv = (function() {
+        $this->csv1 = function() {
             $csv = \fopen(__DIR__.'/../data.csv', 'r');
 
             while ($line = \fgets($csv)) {
                 yield $line;
             }
-        })();
+        };
+        $this->csv2 = function() {
+            $csv = \fopen(__DIR__.'/../data-2.csv', 'r');
+
+            while ($line = \fgets($csv)) {
+                yield $line;
+            }
+        };
     }
 
-    public function testLeNombreDHomme()
+    public function testLeNombreDePersonnesDunMêmeSexe()
     {
         $this->markTestSkipped();
+        $this
+            ->forAll(Set\Elements::of(
+                [$this->csv1, 'homme', 3315],
+                [$this->csv1, 'femme', 6685],
+                [$this->csv2, 'homme', 3349],
+                [$this->csv2, 'femme', 6651],
+            ))
+            ->take(4)
+            ->disableShrinking()
+            ->then(function($data) {
+                [$csv, $sexe, $expected] = $data;
 
-        $this->assertSame(42, $count);
+                $count = 0;
+
+                $this->assertSame($expected, $count);
+            });
     }
 }
